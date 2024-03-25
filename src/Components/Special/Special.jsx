fb8dbb05389ba89c650f5fa41ba4c6a1c18c49b7
Tanvir Ahmad <tnvir9@gmail.com>
2024-03-26 01:02:36 +0600

    How to use Context API to avoid Prop drilling

--- src/Components/Special/Special.jsx
+++ src/Components/Special/Special.jsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
+import { AssetContext } from "../Grandpa/Grandpa";
 
 
 const Special = ({asset}) => {
+    const gift = useContext(AssetContext)
     return (
         <div>
             <h2>Special</h2>
             <p>Asset : {asset}</p>
+            <p>Has also : {gift}</p>
         </div>
     );
 };
